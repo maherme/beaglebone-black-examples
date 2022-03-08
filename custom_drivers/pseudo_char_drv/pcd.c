12404485e467e045e333e65f764c6b1f306b77ad
maherme <manuel.hernandez.mendez@gmail.com>
2022-03-08 22:43:51 +0100

    Added error treatment in init function

--- custom_drivers/pseudo_char_drv/pcd.c
+++ custom_drivers/pseudo_char_drv/pcd.c
@@ -4,6 +4,7 @@
 #include <linux/device.h>
 #include <linux/kdev_t.h>
 #include <linux/uaccess.h>
+#include <linux/err.h>
 
 #define DEV_MEM_SIZE 512
 
@@ -133,8 +134,14 @@ struct device* device_pcd;
 
 static int __init pcd_driver_init(void){
 
+    int ret;
+
     /* Dynamically allocate a device number */
-    alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+    ret = alloc_chrdev_region(&device_number, 0, 1, "pcd_devices");
+    if(ret < 0){
+        pr_err("Alloc chrdev failed\n");
+        goto out;
+    }
 
     pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(device_number), MINOR(device_number));
 
@@ -143,17 +150,46 @@ static int __init pcd_driver_init(void){
 
     /* Register a device (cdev structure) with VFS */
     pcd_cdev.owner = THIS_MODULE;
-    cdev_add(&pcd_cdev, device_number, 1);
+    ret = cdev_add(&pcd_cdev, device_number, 1);
+    if(ret < 0){
+        pr_err("cdev add failed\n");
+        goto unreg_chrdev;
+    }
 
     /* Create device class under /sys/class/ */
     class_pcd = class_create(THIS_MODULE, "pcd_class");
+    /* Check if the pointer is erroneus */
+    if(IS_ERR(class_pcd)){
+        pr_err("Class creation failed\n");
+        /* Convert pointer to error code */
+        ret = PTR_ERR(class_pcd);
+        goto cdev_del;
+    }
 
     /* Populate the sysfs with device information */
     device_pcd = device_create(class_pcd, NULL, device_number, NULL, "pcd");
+    if(IS_ERR(device_pcd)){
+        pr_err("Device create failed\n");
+        ret = PTR_ERR(device_pcd);
+        goto class_del;
+    }
 
     pr_info("Module init was successful");
 
     return 0;
+
+class_del:
+    class_destroy(class_pcd);
+
+cdev_del:
+    cdev_del(&pcd_cdev);
+
+unreg_chrdev:
+    unregister_chrdev_region(device_number, 1);
+
+out:
+    pr_info("Module insertion failed\n");
+    return ret;
 }
 
 static void __exit pcd_driver_cleanup(void){
