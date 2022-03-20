fb9004c5e1f249bc5a3b2f6c6b424af5188e44c2
maherme <manuel.hernandez.mendez@gmail.com>
2022-03-20 19:45:25 +0100

    Usage of devm APIs

--- custom_drivers/pcd_platform_drv/pcd_platform_drv.c
+++ custom_drivers/pcd_platform_drv/pcd_platform_drv.c
@@ -90,10 +90,6 @@ int pcd_platform_driver_remove(struct platform_device* pdev){
     /* Remove a cdev entry from the system */
     cdev_del(&dev_data->cdev);
 
-    /* Free the memory held by the device */
-    kfree(dev_data->buffer);
-    kfree(dev_data);
-
     pcdrv_data.total_devices--;
 
     pr_info("Device is removed\n");
@@ -119,7 +115,7 @@ int pcd_platform_driver_probe(struct platform_device* pdev){
     }
 
     /* Dynamically allocate memory for the device private data */
-    dev_data = kzalloc(sizeof(*dev_data), GFP_KERNEL);
+    dev_data = devm_kzalloc(&pdev->dev, sizeof(*dev_data), GFP_KERNEL);
     if(!dev_data){
         pr_info("Cannot allocate memory\n");
         ret = -ENOMEM;
@@ -138,7 +134,7 @@ int pcd_platform_driver_probe(struct platform_device* pdev){
     pr_info("Device permission = %d\n", dev_data->pdata.perm);
 
     /* Dynamically allocate memory for the device buffer using size information from the platform data */
-    dev_data->buffer = kzalloc(dev_data->pdata.size, GFP_KERNEL);
+    dev_data->buffer = devm_kzalloc(&pdev->dev, dev_data->pdata.size, GFP_KERNEL);
     if(!dev_data->buffer){
         pr_info("Cannot allocate memory");
         ret = -ENOMEM;
@@ -181,10 +177,10 @@ cdev_del:
     cdev_del(&dev_data->cdev);
 
 buffer_free:
-    kfree(dev_data->buffer);
+    devm_kfree(&pdev->dev, dev_data->buffer);
 
 dev_data_free:
-    kfree(dev_data);
+    devm_kfree(&pdev->dev, dev_data);
 
 out:
     pr_info("Device probe failed\n");
