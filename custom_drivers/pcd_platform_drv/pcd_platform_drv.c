70cdfcfefaeda0958f9b14b4d8b5951067eca0c1
maherme <manuel.hernandez.mendez@gmail.com>
2022-04-07 23:38:02 +0200

    Updated lseek, read, write and open functions

--- custom_drivers/pcd_platform_drv/pcd_platform_drv.c
+++ custom_drivers/pcd_platform_drv/pcd_platform_drv.c
@@ -63,27 +63,121 @@ int check_permission(int dev_perm, int acc_mode){
     return -EPERM;
 }
 
-loff_t pcd_lseek(struct file* filp, loff_t offset, int whence){
+loff_t pcd_lseek(struct file* p_file, loff_t offset, int whence){
 
-    return 0;
+    struct pcdev_private_data* pcdev_data = (struct pcdev_private_data*)p_file->private_data;
+    int max_size = pcdev_data->pdata.size;
+    loff_t temp;
+
+    pr_info("lseek requested\n");
+    pr_info("Current value of the file position = %lld\n", p_file->f_pos);
+
+    switch(whence){
+        case SEEK_SET:
+            if((offset > max_size) || (offset < 0)){
+                return -EINVAL;
+            }
+            p_file->f_pos = offset;
+            break;
+        case SEEK_CUR:
+            temp = p_file->f_pos + offset;
+            if((temp > max_size) || (temp < 0)){
+                return -EINVAL;
+            }
+            p_file->f_pos = temp;
+            break;
+        case SEEK_END:
+            temp = max_size + offset;
+            if((temp > max_size) || (temp < 0)){
+                return -EINVAL;
+            }
+            p_file->f_pos = temp;
+            break;
+        default:
+            return -EINVAL;
+    }
+
+    pr_info("New value of the file position = %lld\n", p_file->f_pos);
+
+    return p_file->f_pos;
 }
 
-ssize_t pcd_read(struct file* filp, char __user* buff, size_t count, loff_t* f_pos){
+ssize_t pcd_read(struct file* p_file, char __user* buff, size_t count, loff_t* f_pos){
 
-    return 0;
+    struct pcdev_private_data* pcdev_data = (struct pcdev_private_data*)p_file->private_data;
+    int max_size = pcdev_data->pdata.size;
+
+    pr_info("Read requested for %zu bytes \n", count);
+    pr_info("Current file position = %lld\n", *f_pos);
+
+    if((*f_pos + count) > max_size){
+        count = max_size - *f_pos;
+    }
+
+    if(copy_to_user(buff, pcdev_data->buffer + (*f_pos), count)){
+        return -EFAULT;
+    }
+
+    *f_pos += count;
+
+    pr_info("Number of bytes successfully read = %zu\n", count);
+    pr_info("Updated file position = %lld\n", *f_pos);
+
+    return count;
 }
 
-ssize_t pcd_write(struct file* filp, const char __user* buff, size_t count, loff_t* f_pos){
+ssize_t pcd_write(struct file* p_file, const char __user* buff, size_t count, loff_t* f_pos){
 
-    return -ENOMEM;
+    struct pcdev_private_data* pcdev_data = (struct pcdev_private_data*)p_file->private_data;
+    int max_size = pcdev_data->pdata.size;
+
+    pr_info("Write requested for %zu bytes\n", count);
+    pr_info("Current file position = %lld\n", *f_pos);
+
+    if((*f_pos + count) > max_size){
+        count = max_size - *f_pos;
+    }
+
+    if(!count){
+        pr_err("No space left on the device\n");
+        return -ENOMEM;
+    }
+
+    if(copy_from_user(pcdev_data->buffer + (*f_pos), buff, count)){
+        return -EFAULT;
+    }
+
+    *f_pos += count;
+
+    pr_info("Number of bytes successfully written = %zu\n", count);
+    pr_info("Updated file position = %lld\n", *f_pos);
+
+    return count;
 }
 
-int pcd_open(struct inode* inode, struct file* filp){
+int pcd_open(struct inode* inode, struct file* p_file){
 
-    return 0;
+    int ret;
+    int minor_n;
+    struct pcdev_private_data* pcdev_data;
+
+    /* Find out on which device file open was attempted by the user space */
+    minor_n = MINOR(inode->i_rdev);
+    pr_info("minor access = %d\n", minor_n);
+
+    pcdev_data = container_of(inode->i_cdev, struct pcdev_private_data, cdev);
+
+    /* To supply device private data to other methods of the driver */
+    p_file->private_data = pcdev_data;
+
+    ret = check_permission(pcdev_data->pdata.perm, p_file->f_mode);
+
+    (!ret)?pr_info("Open was successful\n"):pr_info("Open was unsuccessful\n");
+
+    return ret;
 }
 
-int pcd_release(struct inode* inode, struct file* filp){
+int pcd_release(struct inode* inode, struct file* p_file){
 
     pr_info("Release was successful\n");
     return 0;
@@ -198,7 +292,8 @@ struct platform_device_id pcdevs_ids[] = {
     [0] = {.name = "pcdev-A1x", .driver_data = PCDEVA1X},
     [1] = {.name = "pcdev-B1x", .driver_data = PCDEVB1X},
     [2] = {.name = "pcdev-C1x", .driver_data = PCDEVC1X},
-    [3] = {.name = "pcdev-D1x", .driver_data = PCDEVD1X}
+    [3] = {.name = "pcdev-D1x", .driver_data = PCDEVD1X},
+    {} /* Null termination */
 };
 
 struct platform_driver pcd_platform_driver = {
